de025cdd9e7b377cee16902f9002cfe01bfbbc33
Dharmesh <dhmsjoshi@gmail.com>
2026-04-05 18:41:14 +0530

    feat: implement automated email notifications for new user registration

--- notificationService/src/main/java/dev/dharam/notificationservice/kafka/consumer/UserRegistrationConsumer.java
+++ notificationService/src/main/java/dev/dharam/notificationservice/kafka/consumer/UserRegistrationConsumer.java
@@ -2,13 +2,18 @@ package dev.dharam.notificationservice.kafka.consumer;
 
 import dev.dharam.notificationservice.kafka.dto.UserRegisteredEvent;
 import dev.dharam.notificationservice.kafka.topics.KafkaTopics;
+import dev.dharam.notificationservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserRegistrationConsumer {
+
+    private final EmailService emailService;
 
     @KafkaListener(
             topics = KafkaTopics.USER_REGISTRATION,
@@ -20,10 +25,21 @@ public class UserRegistrationConsumer {
         log.info("********** NEW NOTIFICATION RECEIVED **********");
         log.info("User ID: {}", event.userId());
         log.info("Sending Welcome Email to: {}", event.email());
-        log.info("Message Content: {}", event.welcomeMessage());
 
-        // Yahan hum future mein Java Mail Sender (SMTP) integrate karenge
-        log.info("Email successfully sent! (Mocked)");
-        log.info("***********************************************");
+        //call to emailService
+        try{
+            emailService.sendEmail(
+                    event.email(),
+                    "Welcome to Shoppingly!",
+                    event.welcomeMessage()
+            );
+            log.info("Email successfully sent to {}", event.email());
+            log.info("***********************************************");
+        }catch (Exception e){
+            log.error("Error while sending email {}",e.getMessage() );
+        }
+
+
+
     }
 }
---
+++ notificationService/src/main/java/dev/dharam/notificationservice/service/EmailService.java
@@ -0,0 +1,26 @@
+package dev.dharam.notificationservice.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    @Value("${MAIL_USERNAME}")
+    private String email;
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String to, String subject, String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setFrom(email);
+        message.setSubject(subject);
+        message.setText(body);
+        mailSender.send(message);
+    }
+}
